7bd30c837ea557bd6654da1c5337de2f1b31e359
Joost van Stuijvenberg <joostvanstuijvenberg@me.com>
2018-09-16 21:54:03 +0200

    Added some CLion files.

---
+++ cmake-build-debug/CMakeFiles/DemoCV.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/joost/Projects/OpenCV/DemoCV/BasicMorphologyDemo.cpp" "/home/joost/Projects/OpenCV/DemoCV/cmake-build-debug/CMakeFiles/DemoCV.dir/BasicMorphologyDemo.cpp.o"
+  "/home/joost/Projects/OpenCV/DemoCV/BitPlaneSlicingDemo.cpp" "/home/joost/Projects/OpenCV/DemoCV/cmake-build-debug/CMakeFiles/DemoCV.dir/BitPlaneSlicingDemo.cpp.o"
+  "/home/joost/Projects/OpenCV/DemoCV/BlobDetectionDemo.cpp" "/home/joost/Projects/OpenCV/DemoCV/cmake-build-debug/CMakeFiles/DemoCV.dir/BlobDetectionDemo.cpp.o"
+  "/home/joost/Projects/OpenCV/DemoCV/CircularWindowStrategy.cpp" "/home/joost/Projects/OpenCV/DemoCV/cmake-build-debug/CMakeFiles/DemoCV.dir/CircularWindowStrategy.cpp.o"
+  "/home/joost/Projects/OpenCV/DemoCV/ColorDemo.cpp" "/home/joost/Projects/OpenCV/DemoCV/cmake-build-debug/CMakeFiles/DemoCV.dir/ColorDemo.cpp.o"
+  "/home/joost/Projects/OpenCV/DemoCV/Demo.cpp" "/home/joost/Projects/OpenCV/DemoCV/cmake-build-debug/CMakeFiles/DemoCV.dir/Demo.cpp.o"
+  "/home/joost/Projects/OpenCV/DemoCV/DemoCV.cpp" "/home/joost/Projects/OpenCV/DemoCV/cmake-build-debug/CMakeFiles/DemoCV.dir/DemoCV.cpp.o"
+  "/home/joost/Projects/OpenCV/DemoCV/EdgeDetectionDemo.cpp" "/home/joost/Projects/OpenCV/DemoCV/cmake-build-debug/CMakeFiles/DemoCV.dir/EdgeDetectionDemo.cpp.o"
+  "/home/joost/Projects/OpenCV/DemoCV/RegionOfInterestDemo.cpp" "/home/joost/Projects/OpenCV/DemoCV/cmake-build-debug/CMakeFiles/DemoCV.dir/RegionOfInterestDemo.cpp.o"
+  "/home/joost/Projects/OpenCV/DemoCV/ShiftedWindowStrategy.cpp" "/home/joost/Projects/OpenCV/DemoCV/cmake-build-debug/CMakeFiles/DemoCV.dir/ShiftedWindowStrategy.cpp.o"
+  "/home/joost/Projects/OpenCV/DemoCV/SmoothingDemo.cpp" "/home/joost/Projects/OpenCV/DemoCV/cmake-build-debug/CMakeFiles/DemoCV.dir/SmoothingDemo.cpp.o"
+  "/home/joost/Projects/OpenCV/DemoCV/StackedWindowStrategy.cpp" "/home/joost/Projects/OpenCV/DemoCV/cmake-build-debug/CMakeFiles/DemoCV.dir/StackedWindowStrategy.cpp.o"
+  "/home/joost/Projects/OpenCV/DemoCV/ThresholdDemo.cpp" "/home/joost/Projects/OpenCV/DemoCV/cmake-build-debug/CMakeFiles/DemoCV.dir/ThresholdDemo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/DemoCV.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DemoCV.dir/DemoCV.cpp.o"
+  "CMakeFiles/DemoCV.dir/BasicMorphologyDemo.cpp.o"
+  "CMakeFiles/DemoCV.dir/CircularWindowStrategy.cpp.o"
+  "CMakeFiles/DemoCV.dir/EdgeDetectionDemo.cpp.o"
+  "CMakeFiles/DemoCV.dir/ShiftedWindowStrategy.cpp.o"
+  "CMakeFiles/DemoCV.dir/ThresholdDemo.cpp.o"
+  "CMakeFiles/DemoCV.dir/BitPlaneSlicingDemo.cpp.o"
+  "CMakeFiles/DemoCV.dir/ColorDemo.cpp.o"
+  "CMakeFiles/DemoCV.dir/Demo.cpp.o"
+  "CMakeFiles/DemoCV.dir/SmoothingDemo.cpp.o"
+  "CMakeFiles/DemoCV.dir/BlobDetectionDemo.cpp.o"
+  "CMakeFiles/DemoCV.dir/RegionOfInterestDemo.cpp.o"
+  "CMakeFiles/DemoCV.dir/StackedWindowStrategy.cpp.o"
+  "DemoCV.pdb"
+  "DemoCV"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DemoCV.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
