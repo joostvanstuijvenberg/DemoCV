22ae2bdd9df02e7f5b60ceed244b2640bb39a160
Joost van Stuijvenberg <joostvanstuijvenberg@me.com>
2018-09-19 21:23:54 +0200

    Some basic reformatting.

--- BlobDetectionDemo.cpp
+++ BlobDetectionDemo.cpp
@@ -10,8 +10,8 @@
 
 BlobDetectionDemo::BlobDetectionDemo()
 {
-	windows.emplace_back("1. Original");
-	windows.emplace_back("2. Circular blobs");
+    windows.emplace_back("1. Original");
+    windows.emplace_back("2. Circular blobs");
 
     cv::FileStorage storage("../BlobDetectionParameters.xml", cv::FileStorage::READ);
     cv::FileNode node = storage["opencv_storage"];
@@ -26,19 +26,19 @@ BlobDetectionDemo::BlobDetectionDemo()
  */
 void BlobDetectionDemo::demonstrate(Source& source, WindowStrategy& windowStrategy)
 {
-	Demo::demonstrate(source, windowStrategy);
+    Demo::demonstrate(source, windowStrategy);
 
-	std::vector<cv::KeyPoint> keypoints;
+    std::vector<cv::KeyPoint> keypoints;
     cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
 
-	// Original image.
-	windowStrategy.placeWindow(windows[0], original);
+    // Original image.
+    windowStrategy.placeWindow(windows[0], original);
 
-	// Circular blobs.
-	cv::Mat circularBlobs(original.size(), original.type());
-	cv::imshow(windows[1], circularBlobs);
+    // Circular blobs.
+    cv::Mat circularBlobs(original.size(), original.type());
+    cv::imshow(windows[1], circularBlobs);
 
-	detector->detect(original, keypoints);
-	cv::drawKeypoints(original, keypoints, circularBlobs, cv::Scalar(180, 225, 225), cv::DrawMatchesFlags::DEFAULT);
-	windowStrategy.placeWindow(windows[1], circularBlobs);
+    detector->detect(original, keypoints);
+    cv::drawKeypoints(original, keypoints, circularBlobs, cv::Scalar(180, 225, 225), cv::DrawMatchesFlags::DEFAULT);
+    windowStrategy.placeWindow(windows[1], circularBlobs);
 }
